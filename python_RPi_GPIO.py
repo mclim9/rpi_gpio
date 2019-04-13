397b2cda1e8a97ed807960e076d38a61abbef0f2
Your Name <margy@code.com>
2019-04-12 19:05:13 -0700

    ran python

--- python_RPi_GPIO.py
+++ python_RPi_GPIO.py
@@ -3,10 +3,18 @@
 # Speed 70kHz
 
 import RPi.GPIO as GPIO
+import datetime
 
+num = 10000000
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
 
-for x in range(100000):
-    GPIO.output(4, True)
-    GPIO.output(4, False)
+tick = datetime.datetime.now()
+for x in range(num):
+    GPIO.output(4, 1)
+    GPIO.output(4, 0)
+
+tock = datetime.datetime.now() - tick
+timy = tock.seconds + (tock.microseconds/1000000)
+print('Time:%s  %f Hz'%(tock, num/timy))
+
--- python_WiringPi2.py
+++ python_WiringPi2.py
@@ -1,13 +1,19 @@
 # WiringPi
 #   https://github.com/WiringPi
 #   https://github.com/WiringPi/WiringPi-Python
-# Speed: 28kHz
+# RPi3B: 143kHz
 
 import wiringpi2
+import datetime
 
+nummy = 1000000
 io = wiringpi2.GPIO(wiringpi2.GPIO.WPI_MODE_PINS)
 io.pinMode(7,io.OUTPUT)
 
-for x in range(100000):
+tick = datetime.datetime.now()
+for x in range(nummy):
     io.digitalWrite(7,io.HIGH)
     io.digitalWrite(7,io.LOW)
+tock = datetime.datetime.now() - tick
+timy = tock.seconds + tock.microseconds/1000000
+print('Time:%s  %f Hz'%(tock,nummy/timy))
